073e53ab908ce5410290ae321747c6c7797eb5e8
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-02-16 16:02:42 +0100

    Successfully fetched Weather API

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -2,35 +2,47 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 function Weather(){
     const [city, setCity] = useState('')
-    const [data, setData] = useState({})
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=a16375f27891ffa60cb1a9905328304b`
-       const searchLocation  = (event) => {
-        if(event.key === 'Enter'){
-            axios.get(url).then((response) =>{
-                setData(response.data)
-                console.log(response.data)
-            })
-        }
+    const [data, setData] = useState({});
+    //     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}16375f27891ffa60cb1a9905328304b`
+    //    const searchLocation  = (event) => {
+    //     if(event.key === 'Enter'){
+    //         axios.get(url).then((response) =>{
+    //             console(response.data)
+    //             // console.log(response.data)
+    
+// a16375f27891ffa60cb1a9905328304b
+    //         })
+    //     }
         
-       }
+    //    }
         function handleSubmit(event){
             event.preventDefault()
-            setCity(event.target.value)
+        
+        }
+        const handleSearch = () =>{
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=a16375f27891ffa60cb1a9905328304b`)
+            .then(res => res.json())
+            .then((result) => {
+                setData(result)
+                // console.log(result)
+            })
         }
     return(
        <>
         <form onSubmit={handleSubmit}>
             <input type="text" 
             placeholder="Enter City"
-            value={city}
-            // onChange={handleChange}
+            onChange={event => setCity(event.target.value)}
+           
             />
-            <input type="submit" />
+           <button onClick={handleSearch}>Search</button>
         </form>
         {/* <h1>{weather.map(()=>{
             return
         })}</h1> */}
-        <h1>clouds</h1>
+        <h1>{data.name}</h1>
+        <h1>{data.main.temp}</h1>
+        <h1>{data.weather[0].id}</h1>
         </>
     )
 }

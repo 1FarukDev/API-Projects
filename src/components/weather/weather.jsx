9e0468bfc166f5733cbccbce8014b897c5040e0b
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-02-17 12:08:57 +0100

    Started the Styles

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -1,20 +1,8 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import './weather.css'
 function Weather(){
     const [city, setCity] = useState('')
     const [data, setData] = useState({});
-    //     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}16375f27891ffa60cb1a9905328304b`
-    //    const searchLocation  = (event) => {
-    //     if(event.key === 'Enter'){
-    //         axios.get(url).then((response) =>{
-    //             console(response.data)
-    //             // console.log(response.data)
-    
-// a16375f27891ffa60cb1a9905328304b
-    //         })
-    //     }
-        
-    //    }
         function handleSubmit(event){
             event.preventDefault()
         
@@ -24,11 +12,10 @@ function Weather(){
             .then(res => res.json())
             .then((result) => {
                 setData(result)
-                // console.log(result)
             })
         }
     return(
-       <>
+       <main>
         <form onSubmit={handleSubmit}>
             <input type="text" 
             placeholder="Enter City"
@@ -40,10 +27,13 @@ function Weather(){
         {/* <h1>{weather.map(()=>{
             return
         })}</h1> */}
-        <h1>{data.name}</h1>
-        <h1>{data.main.temp}</h1>
-        <h1>{data.weather[0].id}</h1>
-        </>
+        {/* <h1>{data.name} name</h1> */}
+        <h1>Name</h1>
+        <h1>Temp</h1>
+        <h1>Id</h1>
+        {/* <h1>{data.main.temp} temp</h1> */}
+        {/* <h1>{data.weather[0].id} id</h1> */}
+        </main>
     )
 }
 
